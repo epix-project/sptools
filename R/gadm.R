e6f403289a89f99f0432eabb0bd6c1d574d5b9e7
Marc Choisy <marc.choisy@ird.fr>
2018-06-22 16:09:09 +0700

    Adding the gadm function

---
+++ R/gadm.R
@@ -0,0 +1,21 @@
+#' Get GADM data
+#'
+#' Download sp or sf file format from GADM
+#'
+#' @param country character string, name of the country to download
+#' @param format character string, format to downlaod, either sp or sf
+#' @param level integer between 0 and 3 or 4, level of the administrative borders,
+#'              0 being country borders.
+#'
+#' @author Marc Choisy
+#'
+#' @importFrom countrycode countrycode
+#' @export
+gadm <- function(country, format, level) {
+  country <- countrycode(country, "country.name", "iso3c")
+  file <- paste0("gadm36_", country, "_", level, "_", format, ".rds")
+  if (!file.exists(file))
+    download.file(paste0("https://biogeo.ucdavis.edu/data/gadm3.6/R", format, "/", file), file)
+  data <- readRDS(file)
+  data
+}
