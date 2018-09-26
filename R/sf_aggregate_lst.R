f6c6b3bf2d4fcd1b83babc3e0f2695c4e3ec9daa
Lucie Contamin <contamin.lucie@gmail.com>
2018-09-26 16:58:41 +0700

    Integrate complexe event in sf_aggregate_lst fct

--- R/sf_aggregate_lst.R
+++ R/sf_aggregate_lst.R
@@ -36,40 +36,69 @@ select_events <- function(hist_lst, from, to) {
 #' \code{geometry}.
 #' @param event_lst A list containing a list of event, each code with a slot
 #' \code{after}, a slot \code{before}, a slot{event} (split/merge/rename).
-#' @param col_name The name of the columns containing the element to aggregates.
+#' @param col_name The name of the column containing the element to aggregates.
+#' @param col_name2 The name of the 2nd column containing the element to
+#' aggregates for complexe event, by default `NULL`.
 #' @return A object of the same class as \code{df} in which all the provinces
 #' that needed to be merged (according to the time range) are merged.
 #' @keywords internal
 #' @noRd
-aggregate_sf <- function(df, event_lst, col_name) {
+aggregate_sf <- function(df, event_lst, col_name, col_name2 = NULL) {
 
   for (i in seq_along(event_lst)) {
     # select one event
     event <- event_lst[[i]]
 
+    # For the complexe merge event
+    if (event$event == "complexe merge") {
+      suppressWarnings(tmp <-  split(df, f = df[, col_name] %>% unlist %in%
+                                       event$after %>% unlist))
+      # calculate the new geometry and update the new spatial definition (name and
+      # geometry) in the data frame selected
+      tmp$`TRUE` %<>%
+        select(- !! col_name) %>%
+        left_join(event$d.before %>% tidyr::unnest(), by = col_name2) %>%
+        select(!! col_name, !! col_name2, geometry) %>%
+        sf::st_as_sf() %>%
+        group_by(.dots = col_name) %>%
+        st_union(by_feature = TRUE) %>%
+        st_cast("MULTIPOLYGON")
+      # Update the new information in the general data frame
+      df <- rbind(tmp$`TRUE`, tmp$`FALSE`) %>%
+        arrange(!! sym(eval(col_name)))
+    }
+
     # For the split event
-    if (event$event == "split") {
+    if (event$event == "split"| event$event == "complexe split") {
       # Split the data frame to select the province that we need to merge
       # together
-      suppressWarnings(tmp <-  split(df, f = df[, col_name] %>% unlist %in%
-                      event$after %>% unlist))
+      if (event$event == "split") {
+        suppressWarnings(tmp <-  split(df, f = df[, col_name] %>% unlist %in%
+                                         event$after %>% unlist))
+      } else {
+        suppressWarnings(tmp <-  split(df, f = is.element(df[, col_name2] %>%
+                                                 unlist,
+                                                 event$d.after$district %>%
+                                                   unlist %>% na.omit)))
+      }
+
       # calculate the new geometry
       geom <- st_union(tmp$`TRUE`) %>% st_cast("MULTIPOLYGON")
       # Update the new spatial definition (name and geometry) in the data frame
       # selected
-      tmp$`TRUE` %<>% mutate(sel = event$before %>% unlist,
+      tmp$`TRUE` %<>% mutate(new_var = event$before %>% unlist,
                              geometry = geom) %>%
         select(- !! col_name) %>%
         distinct %>%
-        rename(!! col_name := sel)
+        rename(!! col_name := new_var)
       # Update the new information in the general data frame
       df <- rbind(tmp$`TRUE`, tmp$`FALSE`)
     }
 
     # Event rename
     if (event$event == "rename") {
-      df %<>% mutate(col_name = gsub(event$before, event$after, !!col_name)) %>%
-        rename(!! col_name := sel)
+      df %<>% mutate(!! col_name := gsub(event$after, event$before,
+                                         !! sym(eval(col_name))))
     }
   }
   df %>% arrange(!! sym(eval(col_name)))
@@ -90,10 +119,11 @@ aggregate_sf <- function(df, event_lst, col_name) {
 #'  `geometry`
 #'
 #' @param df_sf A sf data frame containing at least the variables
-#' \code{province}, \code{geometry}.
+#' \code{province}, \code{geometry} and \code{district} if `history_lst`
+#' contains complexe event
 #' @param history_lst A list containing a list of event, each code with a slot
-#' \code{after}, a slot \code{before}, a slot{event} (split/merge/rename) and a
-#' slot \code{year}.
+#' \code{after}, a slot \code{before}, a slot{event} (split/merge/rename/
+#' complexe merge/complexe split) and a slot \code{year}.
 #' @param sel an expression on the values of the categorical variable used for
 #' aggregation, by default \code{"province"}
 #' @param from Initial date of the time range selected for the province
@@ -107,9 +137,10 @@ aggregate_sf <- function(df, event_lst, col_name) {
 #' range) are changed.
 #'
 #' @importFrom dplyr select arrange left_join mutate rename distinct contains
-#' sym
+#' sym group_by summarise
 #' @importFrom sf st_union st_cast st_join
 #' @importFrom magrittr %>% %<>%
+#' @importFrom stats na.omit
 #'
 #' @examples
 #'
@@ -121,19 +152,28 @@ aggregate_sf <- function(df, event_lst, col_name) {
 #'                                to = "2004")
 #'
 #' @export
-sf_aggregate_lst <- function(df_sf, history_lst, sel = "province", from,
-                    to = "2018-12-31") {
+sf_aggregate_lst <- function(df_sf, history_lst, from, to = "2018-12-31") {
+
+  if (history_lst %>% purrr::map("event") %>% grepl("complexe", .) %>% any) {
+   sel <- c("province", "district")
+   col_name <- "province"
+   col_name2 <- "district"
+  } else {
+    sel <- "province"
+    col_name <- "province"
+  }
+
   # Prepare the data frame
   df <- select(df_sf, !! sel, geometry)
-  d0 <- as.data.frame(df_sf) %>% select(- contains("geometry"))
   # Select event(s)
   event_lst <- select_events(history_lst, from = from, to = to)
   # Merges back or renames variable(s) together (combine geometry)
-  suppressWarnings(df_agg <- aggregate_sf(df, event_lst, sel))
-  suppressWarnings(df_agg %>% left_join(d0, by = sel))
+  suppressWarnings(df_agg <- aggregate_sf(df, event_lst, col_name,
+                                          col_name2 = col_name2) %>%
+                     group_by(.dots = col_name) %>%
+                     summarise)
 }
 
 ## quiets concerns of R CMD check for the values that appear in pipelines
 if(getRversion() >= "2.15.1")  utils::globalVariables(c(":=", "geometry",
-                                                        "sel"))
-
+                                                        "new_var", "."))
