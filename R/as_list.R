1db2ed8c0154d109a8a3be073ce79daf934480fe
Marc Choisy <marc.choisy@ird.fr>
2018-10-28 17:27:42 +0700

    Adding the as_list function

---
+++ R/as_list.R
@@ -0,0 +1,22 @@
+#' Convert a Spatial polygon into a list of Spatial polygons
+#'
+#' @param plgn a SpatialPolygon*
+#'
+#' @examples
+#' # library(sf)
+#' # library(sptools)
+#' # library(dplyr)
+#' # provinces <- provinces <- "vietnam" %>%
+#' #   sptools::gadm("sf", 1) %>%
+#' #   transmute(province = VARNAME_1) %>%
+#' #   as("Spatial")
+#' # class(provinces)
+#' # list_provinces <- as_list(provinces)
+#' # class(list_provinces)
+#' # table(sapply(list_provinces, class))
+#'
+#' @export
+#'
+as_list <- function(plgn) {
+  lapply(seq_along(plgn), function(x) plgn[x, ])
+}
