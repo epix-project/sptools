a9f8f8b57b987daa6f47509a37fe1129617492cc
Marc Choisy <marc.choisy@ird.fr>
2018-11-08 12:11:48 +0700

    Adding the resample_from_grid function

---
+++ R/resample_from_grid.R
@@ -0,0 +1,56 @@
+#' Resample a Raster object from a Grid
+#'
+#' This function is a wrapper around the \code{\link[raster]{resample}}
+#' function. Instead of resampling a Raster object from another Raster object as
+#' in \code{\link[raster]{resample}}, this function resamples a Raster object
+#' from a \code{SpatialPoints*} grid object. In case of a \code{RasterBrick} or
+#' a \code{RasterStack} object, it resamples only the first layer and returns it
+#' as a \code{RasterLayer} object.
+#'
+#' @param rstr A \code{Raster*} object (i.e. a \code{RasterLayer} or a
+#' \code{RasterBrick} object).
+#' @param grd A \code{SpatialPoint} object with points regularly spaced.
+#'
+#' @return A \code{RasterLayer} object.
+#'
+#' @importFrom magrittr %>%
+#' @importFrom raster rasterFromXYZ raster resample
+#' @importFrom sp spTransform proj4string
+#'
+#' @export
+#'
+#' @examples
+#' # library(sp)
+#' # library(raster)
+#' # library(sptools)
+#' # # The polygon of a country:
+#' # country <- gadm("vietnam", "sp", 0)
+#'
+#' # # A grid of 100 points over the country:
+#' # proj <- proj4string(country)
+#' # grid100 <- country %>%
+#' #   makegrid(100) %>%
+#' #   SpatialPoints(CRS(proj))
+#'
+#' # # Let's resample:
+#' # ppp2010rspld <- resample_from_grid(ppp2010, grid100)
+#'
+#' # # Let's compare:
+#' # ppp2010
+#' # ppp2010rspld
+#' # plot(ppp2010rspld)
+#' # plot(grid100, add = TRUE)
+#' # plot(country, add = TRUE)
+resample_from_grid <- function(rstr, grd) {
+  crs <- proj4string(rstr)
+  layer_or_brick <- function(x) { # this function manages RasterLayer or Bricks (or Stack)
+    if (class(x) == "RasterLayer") return(x)
+    raster(x, 1)
+  }
+  grd %>%
+    spTransform(crs) %>%
+    as.data.frame() %>%
+    rasterFromXYZ(crs = crs) %>%
+    layer_or_brick() %>%
+    resample(rstr, .)
+}
