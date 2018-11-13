edc147daff68ea4a802b697bb2ad98bf56b8803d
Marc Choisy <marc.choisy@ird.fr>
2018-11-13 10:14:58 +0700

    Adding remove_data* functions

---
+++ R/remove_data.R
@@ -0,0 +1,35 @@
+#' Transform SpatialPointsDataFrame into SpatialPoints
+#'
+#' @param spdf A SpatialPointsDataFrame object.
+#'
+#' @return A SpatialPoints object.
+#'
+#' @importFrom sp SpatialPoints CRS proj4string
+#'
+#' @export
+#'
+remove_data_spatialpoints <- function(spdf) {
+  SpatialPoints(spdf, CRS(proj4string(spdf)))
+}
+
+# ------------------------------------------------------------------------------
+
+#' Transform SpatialPolygonsDataFrame into SpatialPolygons
+#'
+#' @param spdf A SpatialPolygonDataFrame object.
+#'
+#' @return A SpatialPolygon object.
+#'
+#' @importFrom sp SpatialPolygons CRS proj4string
+#'
+#' @export
+#'
+#' @examples
+#' vn <- gadm("vietnam", "sp", 1)
+#' vn
+#' remove_data_spatialpolygons(vn)
+#'
+remove_data_spatialpolygons <- function(spdf) {
+  SpatialPolygons(spdf@polygons, spdf@plotOrder, CRS(proj4string(spdf)))
+}
+
