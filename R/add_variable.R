b4d375eae3a9975f223959798218df507e003279
Lucie Contamin <contamin.lucie@gmail.com>
2018-11-13 11:20:57 +0700

    Correct param documentation in add_variable_spdf

--- R/add_variable.R
+++ R/add_variable.R
@@ -1,6 +1,6 @@
 #' Adds variable(s) to a Spatial*DataFrame
 #'
-#' @param sp A Spatial*DataFrame object
+#' @param spdf A Spatial*DataFrame object
 #' @param df A vector or a data frame
 #'
 #' @return An object of the same class than \code{sp}.
@@ -9,17 +9,21 @@
 #'
 #' @examples
 #' library(magrittr)
+#'
 #' vn <- gadm("vietnam", "sp", 1)
 #' vn
 #' vn %<>% add_variable_spdf(rnorm(63))
 #' vn
+#'
 #' # The name of the new variable will be "df". To prevent that we could choose
 #' # the data frame format:
 #' vn %<>% add_variable_spdf(data.frame(normal = rnorm(63)))
 #' vn
+#'
 #' # The data frame format allows the additional possibility to add several
 #' # additional variables all at once:
-#' vn %<>% add_variable_spdf(data.frame(uniform = runif(63), exponential = rexp(63)))
+#' vn %<>% add_variable_spdf(
+#'   data.frame(uniform = runif(63), exponential = rexp(63)))
 #' vn
 add_variable_spdf <- function(spdf, df) {
   spdf@data <- cbind(spdf@data, df)
@@ -43,12 +47,14 @@ add_variable_spdf <- function(spdf, df) {
 #' vn <- gadm("vietnam", "sp", 1)
 #' vn_nodata <- remove_data_spatialpolygons(vn)
 #' vn_nodata
+#'
 #' a <- runif(63)
 #' vn_data <- add_variable_sp(vn_nodata, a)
 #' vn_data
 #'
 #' # An example with a data frame:
-#' vn_data2 <- add_variable_sp(vn_nodata, data.frame(b = rnorm(63), c = rexp(63)))
+#' vn_data2 <- add_variable_sp(vn_nodata,
+#'    data.frame(b = rnorm(63), c = rexp(63)))
 #' vn_data2
 #'
 add_variable_sp <- function(sp, df) {
