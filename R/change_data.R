bf5efe8478f3cc0a507508fb9a895dd7e56027ed
Marc Choisy <marc.choisy@ird.fr>
2018-11-13 11:50:22 +0700

    Adding function change_data

---
+++ R/change_data.R
@@ -0,0 +1,21 @@
+#' Change the data of a Spatial*DataFrame object.
+#'
+#' The aim of this function is to propose an alternative to the bug reported
+#' \href{https://github.com/edzer/sp/issues/56}{here}.
+#'
+#' @param spdf A Spatial*DataFrame object.
+#' @param df A data frame.
+#'
+#' @return An object of the same class as \code{spdf}.
+#'
+#' @export
+#'
+#' @examples
+#' vn <- gadm("vietnam", "sp", 1)
+#' vn
+#' change_data(vn, data.frame(gaussian = rnorm(63), uniform = runif(63)))
+#'
+change_data <- function(spdf, df) {
+  spdf@data <- df
+  spdf
+}
