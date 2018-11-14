b005fc3564a05eb1be751625c8e44a9a701e312b
Marc Choisy <marc.choisy@ird.fr>
2018-11-14 11:03:23 +0700

    Adding function grid2raster

---
+++ R/grid2raster.R
@@ -0,0 +1,31 @@
+#' Convert an SpatialPoints* grid into a Raster*
+#'
+#' @param grid A SpatialPointsDataFrame grid (i.e. where points are regularly
+#'             spaced).
+#'
+#' @return A Raster* object.
+#'
+#' @importFrom raster as.data.frame rasterFromXYZ
+#' @importFrom sp proj4string
+#'
+#' @export
+#'
+#' @examples
+#' library(sf)
+#' vn <- gadmVN::gadm(level = "country")
+#' vn <- sf::as_Spatial(vn)
+#' grid <- make_grid(vn, 100)
+#' grid
+#' grid2raster(grid)
+#'
+#' # An example with data:
+#' grid2 <- add_variable_spts(grid, 1:length(grid))
+#' grid2raster(grid2)
+#'
+#'
+grid2raster <- function(grid) {
+  grid %>%
+    as.data.frame() %>%
+    move_xy() %>%
+    rasterFromXYZ(crs = proj4string(grid))
+}
