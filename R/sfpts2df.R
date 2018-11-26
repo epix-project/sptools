42b03e1a6dbe5d88d393ce604c4ff4e8b91c6a44
Marc Choisy <marc.choisy@ird.fr>
2018-11-26 15:21:29 +0700

    Adding the sfpts2df function

---
+++ R/sfpts2df.R
@@ -0,0 +1,19 @@
+#' Transforms an sf points into a dataframe
+#'
+#' @param sfpts An \code(sf) points object.
+#'
+#' @return A data frame that contains the same fields as \code{sfpts} plus the
+#' longitude and latitude in 2 separate columns.
+#'
+#' @importFrom sf st_geometry<- st_coordinates
+#'
+#' @export
+#'
+#' @examples
+#' sfpts2df(worldpopVN::popcenters2009)
+#'
+sfpts2df <- function(sfpts) {
+  data <- sfpts
+  st_geometry(data) <- NULL
+  data.frame(data, setNames(data.frame(st_coordinates(sfpts)), c("longitude", "latitude")))
+}
