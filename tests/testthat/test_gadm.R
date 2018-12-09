d96e94b2781e492188c9d05c999a9c71d14f60fb
Lucie Contamin <contamin.lucie@gmail.com>
2018-12-09 10:14:37 +0700

    Update tests (gadm interface evolution)

--- tests/testthat/test_gadm.R
+++ tests/testthat/test_gadm.R
@@ -1,15 +1,14 @@
 library(dictionary) # for "XX_history", "XX_province"
+library(magrittr)   # for " %>% ", "is_in
 
 context("`gadm`")
 
 testthat::test_that("`gadm` has the correct behaviour", {
 
-  tmp <- file.path(tempdir(), "pkgtest")
+  tmp <- file.path(tempdir())
   dir.create(tmp)
-  gadm("Cambodia", "sf", 0, file_rm = FALSE, path = tmp)
+  test1 <- gadm("Cambodia", "sf", 0, path = tmp, intlib = FALSE)
 
-  test1 <- dir(tmp)
-
-  testthat::expect_equal(test1, "gadm36_KHM_0_sf.rds")
+  testthat::expect_equal(dir(tmp) %>% is_in("gadm36_KHM_0_sf.rds", .), TRUE)
   unlink(tmp, recursive = TRUE)
 })
--- tests/testthat/test_sf_aggregate_lst.R
+++ tests/testthat/test_sf_aggregate_lst.R
@@ -6,10 +6,13 @@ context("`sf_aggregate_lst`")
 
 test_that("`sf_aggregate_lst` returns the correct output", {
 
-  sf1 <- sptools::gadm("Cambodia", "sf", 1, file_rm = TRUE) %>%
+  tmp <- file.path(tempdir())
+  dir.create(tmp)
+  sf1 <- sptools::gadm("Cambodia", "sf", 1, path = tmp, intlib = FALSE) %>%
     mutate(province = NAME_1 %>% stringi::stri_escape_unicode() %>%
              kh_province[.]) %>%
     select(province, geometry)
+  unlink(tmp, recursive = TRUE)
 
   test1a <- sf_aggregate_lst(sf1, kh_history, from = "1998-01-01")
   expect_equal(match_pattern(test1a %>% as.data.frame, "province",
@@ -21,12 +24,15 @@ test_that("`sf_aggregate_lst` returns the correct output", {
                              kh_province_year),
                "1994-1997")
 
-  sf2 <- sptools::gadm("Laos", "sf", 2, file_rm = TRUE) %>%
+  tmp <- file.path(tempdir())
+  dir.create(tmp)
+  sf2 <- sptools::gadm("Laos", "sf", 2, path = tmp, intlib = FALSE) %>%
     mutate(province = NAME_1 %>% stringi::stri_escape_unicode() %>%
              la_province[.],
            district = NAME_2 %>% stringi::stri_escape_unicode() %>%
              la_district[.]) %>%
     select(province, district, geometry)
+  unlink(tmp, recursive = TRUE)
 
   test2a <- sf_aggregate_lst(sf2, la_history, from = "2008-01-01")
   expect_equal(match_pattern(test2a %>% as.data.frame, "province",
@@ -38,10 +44,13 @@ test_that("`sf_aggregate_lst` returns the correct output", {
                              la_province_year),
                "1997-2006")
 
-  sf3 <- sptools::gadm("Thailand", "sf", 1, file_rm = TRUE) %>%
+  tmp <- file.path(tempdir())
+  dir.create(tmp)
+  sf3 <- sptools::gadm("Thailand", "sf", 1, path = tmp, intlib = FALSE) %>%
     mutate(province = NAME_1 %>% stringi::stri_escape_unicode() %>%
              th_province[.]) %>%
     select(province, geometry)
+  unlink(tmp, recursive = TRUE)
 
   test3 <- sf_aggregate_lst(sf3, th_history, from = "1980-01-01")
   expect_equal(match_pattern(test3 %>% as.data.frame, "province",
